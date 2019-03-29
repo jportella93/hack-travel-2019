b9d461ec0c0d86a30cc1c327a44962e0a8f15346
Edu Aymerich <eduardaymerich@gmail.com>
2019-03-30 00:13:31 +0100

    split screen

--- client/script.js
+++ client/script.js
@@ -18,11 +18,12 @@ function drawResults(searchResults){
     var results = '<table class="table table-striped">'+
         '<thead>'+
           '<tr class="row">'+
-            '<th scope="col" class="col-sm">Name</th>'+
-            '<th scope="col" class="col-sm">From</th>'+
-            '<th scope="col" class="col-sm">To</th>'+
-            '<th scope="col" class="col-sm">Price</th>'+
-            '<th class="col-sm"></th>'+
+            '<th scope="col" class="col-md-3">Name</th>'+
+            '<th scope="col" class="col-md-1">From</th>'+
+            '<th scope="col" class="col-md-1">To</th>'+
+            '<th scope="col" class="col-md-2">Price</th>'+
+            '<th class="col-md-2"></th>'+
+            '<th class="col-md-3"></th>'+
           '</tr>'+
         '</thead>'+
         '<tbody>';
@@ -30,14 +31,12 @@ function drawResults(searchResults){
 
     for(var i=0;i<searchResults.length;i++) {
         results += '<tr class="row">'+
-            '<th scope="row" class="col-sm">Vivian Aranha</th>'+
-            '<td class="col-sm">BCN</td>'+
-            '<td class="col-sm">ATL</td>'+
-            '<td class="col-sm">$290</td>'+
-            '<td class="col-sm">'+
-              '<input type="button" class="btn btn-info btn-small" value="Buy" /> '+
-              '<input type="button" class="btn btn-info btn-small" value="Send to Friend" />'+
-            '</td>'+
+            '<th class="col-md-3">Vivian Aranha</th>'+
+            '<td class="col-md-1">BCN</td>'+
+            '<td class="col-md-1">ATL</td>'+
+            '<td class="col-md-2">$290</td>'+
+            '<td class="col-md-2"><input type="button" class="buy btn btn-info btn-small" value="Buy" /></td>'+
+            '<td class="col-md-3"><input type="button" class="send btn btn-info btn-small" value="Send to Friend" /></td>'+
           '</tr>';
     }
 
@@ -57,9 +56,9 @@ $(document).ready(function () {
         var newRow = $("<tr class='row'>");
         var cols = "";
 
-        cols += '<td class="col-sm"><input type="text" class="form-control" name="name' + counter + '"  placeholder="Full Name"/></td>';
-        cols += '<td class="col-sm"><input type="text" class="form-control" name="airport' + counter + '" placeholder="Airport Code" /></td>';
-        cols += '<td class="col-sm"><input type="button" class="ibtnDel btn btn-md btn-danger"  value="Delete"></td>';
+        cols += '<td class="col-md-5"><input type="text" class="form-control" name="name' + counter + '"  placeholder="Full Name"/></td>';
+        cols += '<td class="col-md-5"><input type="text" class="form-control" name="airport' + counter + '" placeholder="Airport Code" /></td>';
+        cols += '<td class="col-md-2"><input type="button" class="ibtnDel btn btn-md btn-danger" value="Delete"></td>';
         newRow.append(cols);
         $("table.order-list").append(newRow);
         counter++;
@@ -68,7 +67,7 @@ $(document).ready(function () {
 
 
     $("table.order-list").on("click", ".ibtnDel", function (event) {
-        $(this).closest("tr").remove();       
+        $(this).closest("tr").remove();
         counter -= 1
     });
 
